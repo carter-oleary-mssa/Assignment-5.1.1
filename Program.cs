07b519e64e2a2846a456a0e8ebb1fe5cfd8123a8
carter-oleary <carter.m.oleary@outlook.com>
2025-05-05 11:14:21 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿int[] arr = { 121, -121 };
+foreach (var i in arr)
+{
+    Console.WriteLine(IsPalindrome(i));
+}
+
+bool IsPalindrome(int x)
+{
+    string s = x.ToString();
+    for (int i = 0; i < s.Length/2; i++)
+    {
+        if (s[i] != s[s.Length - 1 - i]) return false;
+    }
+    return true;
+}
